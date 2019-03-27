8814a72bd308490b908b79c5e08892fe2298d0ba
ismael8 <ngouaismael154@gmail.com>
2019-03-27 11:52:00 +0100

    act3

--- sprint/php/act3.php
+++ sprint/php/act3.php
@@ -1,24 +1,6 @@
 <?php
-    $jourDeLASemaine[] = "Lundi";
-    $jourDeLASemaine[] = "Mardi";
-    $jourDeLASemaine[] = "Mercrdi";
-    $jourDeLASemaine[] = "Jeudi";
-    $jourDeLASemaine[] = "Vendredi";
-    $jourDeLASemaine[] = "Samedi";
-    $jourDeLASemaine[] = "Dimanche";
-    $jourDeLASemaine[] = "Lundi";
-    $jourDeLASemaine[] = "Mercredi";
-    $jourDeLASemaine[] = "Dimanche";
-
-    echo $jourDeLASemaine[5]
-        
-    //deuxieme forme de tableau
-        
-        <?php
     $jourDeLASemaine = array("Lundi", "Mardi", "Mercredi", "jeudi", "Vendredi", "Samedi", "Dimanche", "Lundi", "Mardi", "Jeudi");
 
     echo $jourDeLASemaine[2];
 
-?>
-   
 ?>
